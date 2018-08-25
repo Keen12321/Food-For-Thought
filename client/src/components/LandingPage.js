098cba7928af26a19033a51dbfb2ba1199618964
Serena Scalzi <serenascalzi@alumni.iu.edu>
2018-08-25 16:12:17 -0700

    built landing page, updated styles, and changed app routes to exact paths

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 class LandingPage extends Component {
  	render() {
-   	return (
- 			<div>
- 				
- 			</div>
-   	)
+	   	return (
+	   		<div className="container">
+	   			<div>
+	   				<h1>Food For Thought</h1>
+	   				<h2>I Want To</h2>
+	   			</div>
+	   			<div className="restaurantButton"> 
+	   				<Link to={"/restaurauntLogin"}><i className="fa fa-cutlery"></i> Donate Food</Link>
+	   			</div>
+	   			<div className="deliveryButton">
+	   				<Link to={"/deliveryLogin"}><i className="fa fa-truck"></i> Receive Food</Link>
+	   			</div>
+	   		</div>
+	   	)
  	}
 }
 
