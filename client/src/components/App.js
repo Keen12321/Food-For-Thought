310df07ce7971bdab35c629988092369e11eb0b4
Mike Michaud <michaud.michael@gmail.com>
2018-08-26 16:10:33 -0700

    removed delivery home import from main page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,6 @@ import { Authentication, AuthRoute } from './Authentication'
 import LandingPage from './LandingPage'
 import DeliveryApp from './delivery/DeliveryApp'
 import RestaurauntApp from './restauraunt/RestaurauntApp'
-import D_Home from './delivery/D_Home'
 
 class App extends Component {
   render () {
@@ -18,7 +17,6 @@ class App extends Component {
 			  		<Route exact path="/" component={LandingPage} />
 			    	<Route exact path="/deliveryLogin" component={DeliveryApp} />
 			    	<Route exact path="/restaurauntLogin" component={RestaurauntApp} />
-			    	<D_Home />
 			    </div>
 		  	</Router>
 		  </Provider>
