1c77ba9f11ee6c540bbf684f35dda56a85b115fc
Mike Michaud <michaud.michael@gmail.com>
2018-08-26 15:38:16 -0700

    added google-maps-react package. started pickups map

--- client/src/components/delivery/D_Map.js
+++ client/src/components/delivery/D_Map.js
@@ -15,7 +15,7 @@ class D_Map extends Component {
 		this.onMapClick = this.onMapClick.bind(this)
 	}
 
-	onMarkerClick = (props, marker, e) => {
+	onMarkerClick = (props, marker, e) => { //shows the infoWindow
 		this.setState({
 			selectedPlace: props,
 			activeMarker: marker,
@@ -23,7 +23,7 @@ class D_Map extends Component {
 		})
 	}
 
-	onMapClick = (props) => {
+	onMapClick = (props) => { //removes infoWindow
 		if(this.state.showingInfoWindow) {
 			this.setState({
 				showingInfoWindow: false,
@@ -37,7 +37,15 @@ class D_Map extends Component {
  		width: '100%',
  		height: '400px'
  	}
+ 	if (!this.props.loaded) {
+ 		return
+ 			<div>
+ 				Loading...
+ 			</div>
+ 	}
  	return (
+ 		<div>
+ 		<D_HomeBar />
    		<Map //the actual google map component from googleAPI
    			item
    			xs = {12}
@@ -45,18 +53,28 @@ class D_Map extends Component {
    			google = {this.props.google}
    			onClick = {this.onMapClick}
    			zoom = {14}
-   			initialCenter = {{lat: 36.1098901, lng: -115.2282014}}>
+   			initialCenter = {{lat: 36.177523, lng: -115.147691}}>
 				<Marker //the red marker on map will represent the delivery home
 					onClick = {this.onMarkerClick}
 					title = {'Las Vegas Rescue Mission'}
-					position = {{lat: 36.1098901, lng: -115.2282014}}
+					position = {{lat: 36.177523, lng: -115.147691}}
 					name = {'Las Vegas Rescue Mission'} />
+				<Marker //the blue marker on map will represent the restaurant home
+					onClick = {this.onMarkerClick}
+					icon = {{color: 'blue'}}
+					title = {'Raku'}
+					position = {{lat: 36.127041, lng: -115.209819}}
+					name = {'Raku'} />
+
 				<InfoWindow
 					marker = {this.state.activeMarker}
 					visible = {this.state.showingInfoWindow}>
-					Info window stuff goes here
+					<p>{this.state.selectedPlace.name}</p>
+					<p>3 main trays</p>
+					<p>4 side trays</p>
 				</InfoWindow>
    		</Map>
+   		</div>
    )
  }
 }
