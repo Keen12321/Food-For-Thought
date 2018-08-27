687e333500e92b97d2c601b442e7a800f1b0dd17
Keen <austin.kuehne@gmail.com>
2018-08-27 09:33:12 -0700

    added new files

---
+++ client/src/components/restaurant/R_HomeBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class R_HomeBar extends Component {
+ render() {
+   return (
+   	<div className="D_homebar">
+   		<Link to='/restauranthome'><i className='fa fa-apple fa-2x'></i>Home</Link>
+   	</div>
+     
+   )
+ }
+}
+
+export default R_HomeBar
