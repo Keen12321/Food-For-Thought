36375f4a5a38d0a5dbb136831a829a8c00c7e244
Keen <austin.kuehne@gmail.com>
2018-08-27 11:12:10 -0700

    fixed routing issues, and spelling errors

--- client/src/components/delivery/DeliveryApp.js
+++ client/src/components/delivery/DeliveryApp.js
@@ -1,30 +1,18 @@
 import React, { Component } from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import { Authentication, AuthRoute } from '../Authentication'
+import {Route} from 'react-router-dom'
 
 import D_Login from './D_Login'
 import D_Register from './D_Register'
-<<<<<<< HEAD
-=======
 import D_Pickups from './D_Pickups'
 import D_Home from './D_Home'
 import D_Reports from './D_Reports'
 import D_Profile from './D_Profile'
->>>>>>> 992697ab2527b9ed7ff84527120a66025dbaf842
+
 
 class RestaurantApp extends Component {
  	render() {
    	return (
-<<<<<<< HEAD
- 			<Authentication
-        redirectUrl='/deliverylogin'
-        defaultRedirect='/'
-      >
-				<Route exact path="/deliverylogin" component={D_Login} />
-				<Route path="/deliveryregister" component={D_Register} />
-      </Authentication>
-=======
-      <div>
+   		<div>
         <Route path="/delivery" component={D_Login} />
         <Route path="/delivery/register" component={D_Register} />
         <Route path="/delivery/home" component={D_Home} />
@@ -32,7 +20,6 @@ class RestaurantApp extends Component {
         <Route path="/delivery/reports" component={D_Reports} />
         <Route path="/delivery/profile" component={D_Profile} />
       </div>
->>>>>>> 992697ab2527b9ed7ff84527120a66025dbaf842
    	)
  	}
 }
