a2e4f24b17fbfeacf0a4c11a881d949666f2cbad
Serena Scalzi <serenascalzi@alumni.iu.edu>
2018-08-27 11:19:13 -0700

    committed changes to pull master

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,10 +3,7 @@ import '../styles/App.css'
 import '../styles/Login.css'
 import store from '../store'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-<<<<<<< HEAD
 import {Provider} from 'react-redux'
-=======
->>>>>>> 992697ab2527b9ed7ff84527120a66025dbaf842
 
 import LandingPage from './LandingPage'
 import DeliveryApp from './delivery/DeliveryApp'
@@ -17,17 +14,10 @@ class App extends Component {
     return (
     	<Provider store = {store}>
 		  	<Router>
-<<<<<<< HEAD
-		  		<div className="mainContainer">
-			  		<Route exactpath="/" component={LandingPage} />
-			    	<Route path="/deliverylogin" component={DeliveryApp} />
-			    	<Route path="/restaurauntLogin" component={RestaurauntApp} />
-=======
 		  		<div>
 			  		<Route exact path="/" component={LandingPage} />
 			    	<Route path="/delivery" component={DeliveryApp} />
 			    	<Route path="/restaurant" component={RestaurantApp} />
->>>>>>> 992697ab2527b9ed7ff84527120a66025dbaf842
 			    </div>
 		  	</Router>
 		  </Provider>
--- client/src/components/delivery/DeliveryApp.js
+++ client/src/components/delivery/DeliveryApp.js
@@ -4,35 +4,27 @@ import { Authentication, AuthRoute } from '../Authentication'
 
 import D_Login from './D_Login'
 import D_Register from './D_Register'
-<<<<<<< HEAD
-=======
 import D_Pickups from './D_Pickups'
 import D_Home from './D_Home'
 import D_Reports from './D_Reports'
 import D_Profile from './D_Profile'
->>>>>>> 992697ab2527b9ed7ff84527120a66025dbaf842
 
 class RestaurantApp extends Component {
  	render() {
    	return (
-<<<<<<< HEAD
  			<Authentication
         redirectUrl='/deliverylogin'
         defaultRedirect='/'
       >
-				<Route exact path="/deliverylogin" component={D_Login} />
-				<Route path="/deliveryregister" component={D_Register} />
+				<div>
+          <Route exact path="/delivery" component={D_Login} />
+          <Route path="/delivery/register" component={D_Register} />
+          <Route path="/delivery/home" component={D_Home} />
+          <Route path="/delivery/pickups" component={D_Pickups} />
+          <Route path="/delivery/reports" component={D_Reports} />
+          <Route path="/delivery/profile" component={D_Profile} />
+        </div>
       </Authentication>
-=======
-      <div>
-        <Route path="/delivery" component={D_Login} />
-        <Route path="/delivery/register" component={D_Register} />
-        <Route path="/delivery/home" component={D_Home} />
-        <Route path="/delivery/pickups" component={D_Pickups} />
-        <Route path="/delivery/reports" component={D_Reports} />
-        <Route path="/delivery/profile" component={D_Profile} />
-      </div>
->>>>>>> 992697ab2527b9ed7ff84527120a66025dbaf842
    	)
  	}
 }
