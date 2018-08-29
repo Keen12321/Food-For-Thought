554bf54926adf6826be8483df220363148e5b5fa
Mike Michaud <michaud.michael@gmail.com>
2018-08-29 10:43:14 -0700

    three button modals

--- client/src/components/delivery/D_Pickups.js
+++ client/src/components/delivery/D_Pickups.js
@@ -38,14 +38,16 @@ class D_Pickups extends Component {
 			<h2>Please confirm this pickup and add it to the map</h2>
 			<p>Restaurant Name</p>
 			<p>Restaurant Address</p>
+			<p>__ Main Trays</p>
+  			<p>__ Side Trays</p>
 			<p>Distance From Location</p>
 			<button className="ui grey button">I don't work</button>
          </Modal>
          <Modal show={this.state.show2} handleClose={this.hideDeleteModal}>
 			<h2>Are you sure you want to delete this pickup?</h2>
 			<p>Restaurant Name</p>
-			<p>Restaurant Address</p>
-			<p>Distance From Location</p>
+			<p>__ Main Trays</p>
+  			<p>__ Side Trays</p>
 			<div className="cxlreason">
 				<textarea placeholder="Must provide reason for cancellation">
 				</textarea>
@@ -64,6 +66,9 @@ class D_Pickups extends Component {
   			</div>
   			<div>
   				<button className="ui green button" onClick={this.showPickupModal}>
+					<i className="map pin icon"></i>Confirm Receipt
+				</button>
+  				<button className="ui blue button" onClick={this.showPickupModal}>
 					<i className="map pin icon"></i>Add to Map
 				</button>
 				<button className="ui red button" onClick={this.showDeleteModal}>
