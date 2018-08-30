1450b837ab1797c92f3e1d899a9c4c1f678ac1b6
Victor Velasquez <victorv24@gmail.com>
2018-08-29 18:15:34 -0700

    updated restaurant nav styles

--- client/src/components/restaurant/R_HomeBar.js
+++ client/src/components/restaurant/R_HomeBar.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import { Container, Icon } from 'semantic-ui-react'
 
 class R_HomeBar extends Component {
    render() {
       return (
       	<div className="navbar">
-      		<Link to="/restaurant"><i className="fa fa-cutlery"></i>Home</Link>
-     		<Link to="/delivery/map"><i className="fa fa-map-marker"></i>Map</Link>
-     		<Link to="/restaurant/profile">Restaurant<i className="fa fa-user"></i></Link>         
+      		<Link to="/restaurant">
+      			<Icon.Group size='large'>
+      				<Icon size='big' fitted name='circle outline' />
+      				<Icon name='food' />
+    				</Icon.Group>
+    				<p>Home</p>
+    			</Link>
+	     		<Link to="/delivery/map">
+	     			<Icon.Group size='large'>
+      				<Icon size='big' fitted name='circle outline' />
+      				<Icon name='map marker alternate' />
+    				</Icon.Group>
+    				<p>Map</p>
+	     		</Link>
+	     		<Link to="/restaurant/profile">
+	     			<Icon.Group size='large'>
+      				<Icon size='big' fitted name='circle outline' />
+      				<Icon name='user' />
+    				</Icon.Group>
+    				<p>Profile</p>
+	     		</Link>         
       	</div>
       )
    }
@@ -15,4 +34,4 @@ class R_HomeBar extends Component {
 
 export default R_HomeBar
 
-// https://github.com/Keen12321/Food-For-Thought/pull/34/conflict?name=client%252Fsrc%252Fcomponents%252Frestaurant%252FR_HomeBar.js&base_oid=d0985bc7f2dd4fb9c376451f6466db18bee5e194&head_oid=255fa1dc2799b9530093d4c5fdb79d895d83c6b4
+// <Link to="/restaurant"><i className="fa fa-cutlery"></i>Home</Link>
