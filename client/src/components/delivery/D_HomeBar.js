60fdc2d3e6bc7777db73a251330e8a63eb5329c1
Keen <austin.kuehne@gmail.com>
2018-08-30 14:08:48 -0700

    working on login button

--- client/src/components/delivery/D_HomeBar.js
+++ client/src/components/delivery/D_HomeBar.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import {api} from '../Authentication'
+import { Dropdown } from 'semantic-ui-react'
 
 class D_HomeBar extends Component {
+  render() {
+    const trigger = (
+      <span>
+        {api.getProfile().name}
+      </span>
+    )
 
- render() {
-   return (
-   	<div className="D_navbar">
-   		<div className="D_navbar left">
-   			<Link to="/delivery/home"><i className="fa-nav fa fa-truck" />Home</Link>
-   			<Link to="/delivery/map">Map</Link>
-   			<Link to="/delivery/pickups">Pickups</Link>
-   		</div>
-   		<div className="D_navbar right">
-   			<Link to="/delivery/profile">LV Rescue Mission<i className="fa-nav fa fa-user" /></Link>
-   		</div>
-   	</div>
-     
-   )
- }
+    const options = [
+      { key: 'user', text: 'Account', icon: 'user' },
+      { key: 'sign-out', text: 'Sign Out', icon: 'sign out' },
+    ]
 
+    return (
+    	<div className="D_navbar">
+    		<div className="D_navbar left">
+    			<Link to="/delivery"><i className="fa-nav fa fa-truck" />Home</Link>
+    			<Link to="/delivery/map">Map</Link>
+    			<Link to="/delivery/pickups">Pickups</Link>
+    		</div>
+    		<div className="D_navbar right">
+    			<Dropdown trigger={trigger} options={options} pointing='top right' icon={null} />
+    		</div>
+    	</div>
+    )
+  }
 }
 
 export default D_HomeBar
