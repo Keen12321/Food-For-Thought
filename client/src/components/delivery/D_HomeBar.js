f54c2be1808d31d084ee160c26574a854e389a8c
Keen <austin.kuehne@gmail.com>
2018-08-31 14:27:42 -0700

    partially changed login to semantic UI

--- client/src/components/delivery/D_HomeBar.js
+++ client/src/components/delivery/D_HomeBar.js
@@ -12,32 +12,26 @@ class D_HomeBar extends Component {
   render() {
     const trigger = (
       <span>
-        Account
+        Welcome, {api.getProfile().name}
       </span>
     )
 
     return (
-    	<div className="D_navbar">
-    		<div className="D_navbar left">
-    			<Link to="/delivery"><i className="fa-nav fa fa-truck" />Home</Link>
-    			<Link to="/delivery/map">Map</Link>
-    			<Link to="/delivery/pickups">Pickups</Link>
-    		</div>
-    		<div className="D_navbar right">
-    			<Dropdown trigger={trigger} pointing='top right' icon={null}>
+      <div className="D_navbar">
+        <div className="D_navbar left">
+          <Link to="/delivery"><i className="fa-nav fa fa-truck" />Home</Link>
+          <Link to="/delivery/map">Map</Link>
+          <Link to="/delivery/pickups">Pickup</Link>
+        </div>
+        <div className="D_navbar right">
+          <Dropdown trigger={trigger} pointing='top right' icon={null}>
             <Dropdown.Menu>
-              <Dropdown.Item key='username'>
-                <span>
-                  Signed in as <strong>{api.getProfile().name}</strong>
-                </span>
-              </Dropdown.Item>
-              <Dropdown.Divider />
               <Dropdown.Item as={Link} to='/delivery/profile' key='user' text='Edit Profile' icon='user'/>
               <Dropdown.Item as={Link} to='/login' onClick={this.logout} key='sign-out' text='Sign Out' icon='sign out' />   
             </Dropdown.Menu>
           </Dropdown>
-    		</div>
-    	</div>
+        </div>
+      </div>
     )
   }
 }
--- client/src/components/delivery/DeliveryApp.js
+++ client/src/components/delivery/DeliveryApp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Route, Switch} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import { Authentication } from '../Authentication'
 
 import HomeBar from './D_HomeBar'
@@ -16,16 +16,12 @@ class DeliveryApp extends Component {
         redirectUrl='/delivery'
         defaultRedirect='/'
       >
-        <div>
-          <HomeBar />
-          <Switch>
-            <Route exact path="/delivery" component={D_Home} />
-            <Route path="/delivery/pickups" component={D_Pickups} />
-            <Route path="/delivery/reports" component={D_Reports} />
-            <Route path="/delivery/profile" component={D_Profile} />
-            <Route path="/delivery/map" component={D_Map} />
-          </Switch>
-        </div>
+        <HomeBar />
+        <Route exact path="/delivery" component={D_Home} />
+        <Route path="/delivery/pickups" component={D_Pickups} />
+        <Route path="/delivery/reports" component={D_Reports} />
+        <Route path="/delivery/profile" component={D_Profile} />
+        <Route path="/delivery/map" component={D_Map} />
       </Authentication>
     )
   }
