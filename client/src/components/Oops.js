f55ee0c218b566632a46dfaea3b0dab328d8b9ad
Mike Michaud <michaud.michael@gmail.com>
2018-09-02 21:52:21 -0700

    assets folder with image for 404 page

--- client/src/components/Oops.js
+++ client/src/components/Oops.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 import '../styles/App.css'
 import {Link} from 'react-router-dom'
 import {Button} from 'semantic-ui-react'
+import logo from '../assets/404.png'
 
 
 class Oops extends Component {
  render() {
    return (
    		<div className="errorpage">
-   			<span className="dubdub">404</span>
-   			<span className="schwifty">It ain't here!</span>
+   		<img src={logo} />
    		</div>
      
    )
