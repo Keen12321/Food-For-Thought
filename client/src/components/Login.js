2acf421044cbc3e36cbe1e65606eb61f9715c8da
Victor Velasquez <victorv24@gmail.com>
2018-09-04 16:16:51 -0700

    user icon changes color for validation

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -20,9 +20,11 @@ class D_Login extends Component {
 		this.setState({
 			[e.target.name]:e.target.value
 		})
+		
 		if(this.state.email !== '' && this.state.password !== '') {
 			this.setState({
-				buttonColor:'blue'
+				buttonColor: 'blue',
+				iconColor: 'green'
 			})
 		}
 
@@ -79,11 +81,21 @@ class D_Login extends Component {
 	   			</div>
 
 	 				<Form size='huge' className="loginForm" onSubmit={this.handleSubmit}>
-	 					<Form.Input className='red' fluid icon='user'  iconPosition='left' name='email' type='email' onChange={this.handleChange} value={this.state.email} placeholder='Email' />
+	 					<Form.Input className='red' name='email' type='email'
+	 						fluid icon={<Icon name='user' color={this.state.iconColor} />} iconPosition='left' 
+	 						onChange={this.handleChange} value={this.state.email} 
+	 						placeholder='Email' />
 	 					{incorrectEmailValidation}
-	 					<Form.Input fluid icon='key' iconPosition='left' name='password' onChange={this.handleChange} value={this.state.password} placeholder='Password' type='password' />
-	 					<Button className="loginSubmit" type="submit" color={this.state.buttonColor} size='big'>LOGIN</Button>
-	 					<Link className="registrationLink" to="/register">Not a Registered User? Register Here</Link>
+
+	 					<Form.Input fluid icon='key' iconPosition='left' name='password' 
+	 						onChange={this.handleChange} value={this.state.password} 
+	 						placeholder='Password' type='password' />
+	 					
+	 					<Button className="loginSubmit" type="submit" 
+	 						color={this.state.buttonColor} size='big'>LOGIN</Button>
+
+	 					<Link className="registrationLink" to="/register">
+	 						Not a Registered User? Register Here</Link>
 	 				</Form>
 	 			</div>
 	   	)
