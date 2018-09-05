bb4d485a885b5ca52f2e6535e58fed922db67c6f
Victor Velasquez <victorv24@gmail.com>
2018-09-05 10:02:46 -0700

    refactoring code, beginning to debug...

--- client/src/components/delivery/D_Home.js
+++ client/src/components/delivery/D_Home.js
@@ -1,18 +1,18 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
-import {withAuth, api} from '../Authentication'
-import {connect} from 'react-redux'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { withAuth, api } from '../Authentication'
+import { connect } from 'react-redux'
 import { getDonations } from '../../actions/donateActions'
-import {Container, Button, Header} from 'semantic-ui-react'
+import { Container, Button, Header } from 'semantic-ui-react'
 
 class D_Home extends Component {
   state = {
-      id:api.getProfile().id
-   }
+    id:api.getProfile().id
+  }
 
-   componentDidMount() {
+  componentDidMount() {
     getDonations()
-   }
+  }
 
   componentWillReceiveProps(newProps) {
     if ( this.props.donate !== newProps.donate)  {
@@ -26,23 +26,31 @@ class D_Home extends Component {
     return (
       <div>
         <div className="pickupnotification">
-          <Header as='h3'>Pickups available for today: {this.props.donate.length}</Header>
+          <Header as='h3'>
+            Pickups available for today: {this.props.donate.length}
+          </Header>
         </div>
      		
         <Container className="D_HomeContainer">
      			<div>
      				<Link to={`/delivery/map/${this.state.id}`}>
-              <Button color='green' type="submit" className="startPickup wubba">My Pickups Map</Button>
-              </Link>
+              <Button color='green' type="submit" className="startPickup wubba">
+                My Pickups Map
+              </Button>
+            </Link>
      			</div>
      			<div>
      				<Link to={`/delivery/reports/${this.state.id}`}>
-              <Button color='red' type="submit" className="manageReports wubba">Manage Reports</Button>
+              <Button color='red' type="submit" className="manageReports wubba">
+                Manage Reports
+              </Button>
             </Link>
      			</div>
      			<div>
      				<Link to="/delivery/pickups">
-              <Button color='blue' type="submit" className="navHome wubba">View All Available Pickups</Button>
+              <Button color='blue' type="submit" className="navHome wubba">
+                View All Available Pickups
+              </Button>
             </Link>
      			</div>
      		</Container>
@@ -57,4 +65,5 @@ function mapStateToProps(appState) {
     donate: appState.appReducer.donate
   }
 }
+
 export default withAuth(connect(mapStateToProps)(D_Home))
--- client/src/components/delivery/D_Map.js
+++ client/src/components/delivery/D_Map.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react'
 // import D_Pickups from './D_Pickups'
 import  { compose, withProps, lifecycle } from 'recompose'
-import {withScriptjs, withGoogleMap, GoogleMap, TrafficLayer, DirectionsRenderer} from 'react-google-maps'
+import { withScriptjs, withGoogleMap, GoogleMap, TrafficLayer, DirectionsRenderer } from 'react-google-maps'
 import { withAuth, api } from '../Authentication'
 import { connect } from 'react-redux'
 import { getAddresses } from '../../actions/donateActions'
@@ -46,62 +46,65 @@ class D_Map extends Component {
     }
   }
 
-render() {
-  const lat = this.state.currentLatLng.lat
-  const lng = this.state.currentLatLng.lng
-  const waypnt = this.props.addresses
-  var rte
+  render() {
+    const lat = this.state.currentLatLng.lat
+    const lng = this.state.currentLatLng.lng
+    const waypnt = this.props.addresses
+    let rte
 
-  const DirectionsComponent = compose(
+    const DirectionsComponent = compose(
 
-    withProps({
-      googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDNIsEsuc8FsHQJsswUcDKUd9k3sZqzk3U",
-      loadingElement: <div style={{ height: `100%` }} />,
-      containerElement: <div style={{ width: `100%` }} />,
-      mapElement: <div style={{height: `600px`, width: `100%` }}  />,
-    }),
+      withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDNIsEsuc8FsHQJsswUcDKUd9k3sZqzk3U",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ width: `100%` }} />,
+        mapElement: <div style={{height: `600px`, width: `100%` }}  />,
+      }),
 
-    withScriptjs,
-    withGoogleMap,
+      withScriptjs,
+      withGoogleMap,
 
-    lifecycle({
-      componentDidMount() { 
-        const DirectionsService = new google.maps.DirectionsService()
-        DirectionsService.route({
-          origin: new google.maps.LatLng({lat:lat, lng:lng}),
-          destination: api.getProfile().location,
-          waypoints: waypnt, //empty array is ok. array of objects with location and stopover
-          optimizeWaypoints: true,
-          travelMode: google.maps.TravelMode.DRIVING,
-        }, (result, status) => {
-          if (status === google.maps.DirectionsStatus.OK) {
-            this.setState({
-              directions: {...result},
-            })
-            rte = {...result.routes}
-            console.log('asdf', rte)
-          }
-           else {
-            console.error(`error fetching directions ${result}`)
-          }
-        })
-      }
-    })
+      lifecycle({
+        componentDidMount() { 
+          const DirectionsService = new google.maps.DirectionsService()
+          
+          DirectionsService.route({
+            origin: new google.maps.LatLng({lat:lat, lng:lng}),
+            destination: api.getProfile().location,
+            waypoints: waypnt, //empty array is ok. array of objects with location and stopover
+            optimizeWaypoints: true,
+            travelMode: google.maps.TravelMode.DRIVING
+          }, (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+              this.setState({
+                directions: {...result}
+              })
 
-  )(props =>
-    <GoogleMap defaultZoom={8} center={{lat: 36.1699, lng: -115.1398}}>
-         {props.directions && <DirectionsRenderer directions={props.directions}  />}
-         <TrafficLayer autoUpdate />
-    </GoogleMap>
-  )
-  console.log('dddd', this.props)
-return (
-	   <div className="pickupsContainer">
-	   <div id="scroll">
-        <Pickups />
+              rte = {...result.routes}
+              console.log('asdf', rte)
+            }
+             else {
+              console.error(`error fetching directions ${result}`)
+            }
+          })
+        }
+      })
+    )( props =>
+      <GoogleMap defaultZoom={8} center={{lat: 36.1699, lng: -115.1398}}>
+        {props.directions && <DirectionsRenderer directions={props.directions} />}
+        <TrafficLayer autoUpdate />
+      </GoogleMap>
+    )
+    console.log('dddd', this.props)
+  
+    return (
+      <div className="pickupsContainer">
+        <div id="scroll">
+          <Pickups />
         </div>
+        
         <DirectionsComponent />
-     </div>
+      </div>
     )
   }
 }
@@ -111,6 +114,5 @@ function mapStateToProps(appState) {
 		addresses: appState.appReducer.addresses
 	}
 }
+
 export default withAuth(connect(mapStateToProps)(D_Map))
-
-
